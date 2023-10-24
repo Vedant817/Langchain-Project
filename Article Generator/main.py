4577c4f04cab41df6618bafec10316b36dc69c58
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-10-24 15:28:51 +0530

    Connecting to Langchain and OpenAI for generating the response.

--- Article Generator/main.py
+++ Article Generator/main.py
@@ -1,4 +1,13 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template, request
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
+import openai
+
+load_dotenv()
+API_KEY = os.getenv('OPENAI_API_KEY')
 
 app = Flask(__name__)
 
@@ -6,10 +15,16 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/generate', method=['GET', 'POST'])
+@app.route('/generate', methods=['GET','POST'])
 def generate():
     if request.method == 'POST':
-        output = request.form
-    return jsonify(output)
+        llm = OpenAI(openai_api_key=API_KEY,temperature = 0.7)
+        prompt = PromptTemplate.from_template("Write an Article on {title}")
+        title = request.json.get('prompt') #! allows you to access JSON data sent in an HTTP request.
+        #? request.form-> The key/value pairs in the body, from a HTML post form, or JavaScript request that isn't JSON encoded.
+        chain = LLMChain(llm=llm, prompt=prompt)
+        output = chain.run(title)
+    return output
 
-app.run(host='0.0.0.0', port=3000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
